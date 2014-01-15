b4c236e6109de4ff0826919ff097089effa08cb9
giacomos <giacomo.spettoli@gmail.com>
2014-01-15 18:44:27 +0100

    fix brightness percentage

--- conky/brightnesslevel.sh
+++ conky/brightnesslevel.sh
@@ -2,11 +2,11 @@
 
 current_brig=`pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness`
 max_brig=`pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-max-brightness`
-
+percent=`echo $current_brig*100/$max_brig| bc`
 
 case "$1" in
     "get")
-        echo "$current_brig%"
+        echo "$percent%"
         ;;
     "set")
         pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $2
