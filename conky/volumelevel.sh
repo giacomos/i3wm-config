5966de7e8e8965388eaced7c4510639ff272a26b
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2014-02-03 17:26:23 +0100

    fix volumelevel script

--- conky/volumelevel.sh
+++ conky/volumelevel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-vol=`amixer get Master | awk -F'[]%[]' '/%/ {if ($5 == "off") { print "Mute" } else { print $2"%" }}' | uniq | head -1`
+vol=`amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "Mute" } else { print $2"%" }}' | uniq | head -1`
 echo "$vol"
