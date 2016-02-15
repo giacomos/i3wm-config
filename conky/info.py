33d93526910ebb7dd3d6475cadfbe484bf8b786e
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-02-15 13:05:27 +0100

    switch from dmenu to rofi

--- conky/info.py
+++ conky/info.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import sys
-import NetworkManager as nm
+#import NetworkManager as nm
 import argparse
 
 
-def get_ip_address(ifname):
-    for dev in nm.NetworkManager.GetDevices():
-        if dev.Interface == ifname:
-            if dev.State == 20:
-                return '<hw Disabled>'
-            elif dev.State == 30:
-                return '<not connected>'
-            else:
-                return dev.Ip4Address
-    return '<not found>'
+# def get_ip_address(ifname):
+#     for dev in nm.NetworkManager.GetDevices():
+#         if dev.Interface == ifname:
+#             if dev.State == 20:
+#                 return '<hw Disabled>'
+#             elif dev.State == 30:
+#                 return '<not connected>'
+#             else:
+#                 return dev.Ip4Address
+#     return '<not found>'
 
 parser = argparse.ArgumentParser(description='Provides some useful infos about the system.')
 
@@ -33,10 +33,10 @@ parser.add_argument('--get-keyboard-layout',
                     action='store_true',
                     dest='get_keyboard_layout',
                     help="show keyboard layout infos")
-parser.add_argument('--get-ip',
-                    action='store',
-                    dest='get_ip',
-                    help="get ip for interface")
+# parser.add_argument('--get-ip',
+#                     action='store',
+#                     dest='get_ip',
+#                     help="get ip for interface")
 parser.add_argument('--get-volume',
                     action='store_true',
                     dest='get_volume',
@@ -69,9 +69,9 @@ elif args.set_brightness:
     command = 'pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness %s'
     p = subprocess.Popen(command % new_b, shell=True,
                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-elif args.get_ip:
-    interface = args.get_ip
-    sys.stdout.write(get_ip_address(interface))
+# elif args.get_ip:
+#     interface = args.get_ip
+#     sys.stdout.write(get_ip_address(interface))
 elif args.get_keyboard_layout:
     command = 'setxkbmap -print | grep xkb_symbols | awk \'{print $4}\' | awk -F"+" \'{print $2}\''
     p = subprocess.Popen(command, shell=True,
--- conky/volumelevel.sh
+++ conky/volumelevel.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-vol=`amixer get IEC958 | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "Mute" } else { print $2"%" }}' | uniq | head -1`
-echo "$vol"
+pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 2 )) | tail -n 1| sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
